be2acb3b91680fd95a691689df496c96c15ecc1f
tanshaohui <tanshaohui@baidu.com>
2016-02-26 18:07:15 +0800

    Commit

--- ads.js
+++ ads.js
@@ -4,6 +4,8 @@ var init = function () {
     }
 };
 
+init();
+
 var triggerEvent = function (dom, type) {
     // 创建
     var evt = document.createEvent('HTMLEvents');
@@ -16,23 +18,29 @@ var triggerEvent = function (dom, type) {
 };
 
 var isNotUrl = function (str) {
-    if (!str || str.indexOf('javascript:') == 0) {
+    if (!str || str.indexOf('javascript:') === 0) {
         return true;
     }
     return false;
 };
 
-var findAds = function (width, height) {
-    var $img = $('img,embed,object').filter(function (index, item) {
+var findAdsWithSize = function (width, height, context) {
+    if (!context) {
+        context = $('html');
+    }
+    return context.find('img,embed,iframe,object').filter(function (index, item) {
         return $(item).width() == width && $(item).height() == height;
     });
-    
-    var $a = $img.siblings('a');
-    if ($a.length == 0) {
-        $a = $img.parents('a');
-    }
+};
 
-    return $a;
+var findAdsWithDomain = function (domain, context) {
+    if (!context) {
+        context = $('html');
+    }
+    return context.find('embed,iframe').filter(function (index, item) {
+        var src = $(item).attr('src');
+        return src && src.indexOf(domain) > -1 && src.indexOf(domain) < 9;
+    });
 };
 
 var openAds = function (href) {
@@ -41,32 +49,58 @@ var openAds = function (href) {
     $iframe.get(0).src = href;
 };
 
-var autoOpenAds = function (width, height) {
-    var $a = findAds(width, height);
+var openIframeAds = function (src) {
+    var $iframe = $('<iframe height=0 width=0 />');
+    $('body').append($iframe);
+    $iframe.on('load', function (e) {
+        var $html = $(this).contents();
+        var arg = [];
+        for (var i = 1; i < arguments.length; i ++) {
+            arg.push(arguments[i]);
+        }
+        autoOpenAds.apply(arg.concat($html));
+    });
+    $iframe.get(0).src = '/PHPProxy/phpproxy.php?url=' + src;
+};
 
-    if ($a.length < 1) {
+var autoOpenAds = function (width, height, context) {debugger;
+    var $ads = findAdsWithSize(width, height, context);
+
+    if ($ads.length < 1) {
         return false;
     }
 
-    if (!isNotUrl($a.attr('href'))) {
-        openAds($a.attr('href'));
+    if ($ads.get(0).tagName === 'IFRAME') {
+        openIframeAds($ads.attr('src'), width, height);
+        if ($ads.parent().get(0).tagName === 'BODY') {
+            $ads.remove();
+        } else {
+            $ads.parent().remove();
+        }
     } else {
-        var wOpen = window.open;
-        window.open = openAds;
-        triggerEvent($a.get(0), 'click');
-        window.open = wOpen;
-    }
+        var $a = $img.siblings('a');
+        if ($a.length === 0) {
+            $a = $img.parents('a');
+        }
 
-    if ($a.parent().get(0).tagName == 'BODY') {
-        $a.remove();
-    } else {
-        $a.parent().remove();
+        if (isNotUrl($a.attr('href'))) {
+            var wOpen = window.open;
+            window.open = openAds;
+            triggerEvent($a.get(0), 'click');
+            window.open = wOpen;
+        } else {
+            openAds($a.attr('href'));
+        }
+
+        if ($a.parent().get(0).tagName === 'BODY') {
+            $a.remove();
+        } else {
+            $a.parent().remove();
+        }
     }
 
     return true;
 };
-
-init();
 
 setTimeout(function () {
     if (!autoOpenAds(300, 250)) {
@@ -74,8 +108,8 @@ setTimeout(function () {
     }
 }, 1000);
 
-setTimeout(function () {
-    if (!autoOpenAds(728, 90)) {
-        setTimeout(arguments.callee, 1000);
-    }
-}, 1000);
+// setTimeout(function () {
+//     if (!autoOpenAds(728, 90)) {
+//         setTimeout(arguments.callee, 1000);
+//     }
+// }, 1000);
